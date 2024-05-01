6bac27603aeb3ed871eb1e92b0ca703964ba0c4d
Rafiklatrikdu69 <rafik.bouchenna050@email.com>
2024-05-01 15:52:53 +0200

    modif composant techno

---
+++ portfoliot-front/src/app/components/card-techno/card-techno.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Technology } from '../../entity/Technology';
+
+@Component({
+  selector: 'app-card-techno',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-techno.component.html',
+  styleUrl: './card-techno.component.css'
+})
+export class CardTechnoComponent {
+  
+@Input()techno!:Technology;
+
+}
--- portfoliot-front/src/app/components/categorie-techno-card/categorie-techno-card.component.ts
+++ portfoliot-front/src/app/components/categorie-techno-card/categorie-techno-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Categorie } from '../../entity/Categorie';
 import { CommonModule } from '@angular/common';
+import { CardTechnoComponent } from '../card-techno/card-techno.component';
 
 @Component({
   selector: 'app-categorie-techno-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,CardTechnoComponent],
   templateUrl: './categorie-techno-card.component.html',
   styleUrl: './categorie-techno-card.component.css'
 })
